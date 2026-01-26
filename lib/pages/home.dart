2996a45d77965e97887c42c8f18462e9d0286285
Thierryshine <thierryndayishemeze7@gmail.com>
2026-01-26 22:40:11 +0200

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "App Manager",
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: const Color.fromARGB(255, 33, 129, 24),
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
