147ce7a22a3d5f3e0a3996f45e7bef2471a0ad83
Thierryshine <thierryndayishemeze7@gmail.com>
2026-02-15 01:45:53 +0200

    commit changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,7 +5,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var height = 40;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -19,12 +18,14 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         leading: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Image.asset("img/arrow.png",width: 35,height: 35),
-        ) //La partie gauche de l'AppBar
-        actions: [Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Image.asset("img/settings.png",width: 40,height: 40 ),
-        ),],
+          child: Image.asset("img/arrow.png", width: 35, height: 35),
+        ), //La partie gauche de l'AppBar
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Image.asset("img/settings.png", width: 40, height: 40),
+          ),
+        ],
         backgroundColor: const Color.fromARGB(255, 248, 248, 173),
       ),
     );
